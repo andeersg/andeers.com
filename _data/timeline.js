175ddd9d445b3ffb86b002d534c596c0d2c0f9b9
Anders Grendstadbakk <anders.2205@gmail.com>
2019-01-12 13:41:40 +0100

    Timeline data file

---
+++ _data/timeline.js
@@ -0,0 +1,7 @@
+module.exports = [
+  {
+    type: '', // @NOTE Could be used for colors
+    activity: '', // @NOTE If need for it, add description field for extra details.
+    date: '', // @TODO Find a format that is easy to write for ordering stuff. Maybe add additional field for display.
+  }
+];
