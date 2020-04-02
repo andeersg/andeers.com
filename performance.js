9a79568a29f7e39bc4abb170978ee6d4b9e7757c
Anders Grendstadbakk <anders.2205@gmail.com>
2020-04-02 15:52:59 +0200

    Att performance check script

---
+++ performance.js
@@ -0,0 +1,14 @@
+const PerfLeaderboard = require("performance-leaderboard");
+
+(async function() {
+
+	let urls = [
+    "https://andeers.com",
+    "http://localhost:8081"
+	];
+
+	console.log( await PerfLeaderboard(urls) );
+
+	// Run each site 5 times (default is 3)
+	// console.log( await PerfLeaderboard(urls, 5) );
+})();
