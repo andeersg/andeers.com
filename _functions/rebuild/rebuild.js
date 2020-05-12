71566fc697aa91c6c507053016299a0be4005b60
Anders Grendstadbakk <anders.2205@gmail.com>
2020-05-12 15:10:53 +0200

    Test function

--- _functions/rebuild/rebuild.js
+++ _functions/rebuild/rebuild.js
@@ -3,23 +3,25 @@ const fs = require('fs');
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-    const token = event.queryStringParameters.token || false;
-    if (token !== process.env.REBUILD_TOKEN) {
-      return {
-        statusCode: 200,
-        body: '{}',
-      }
-    }
+    // const token = event.queryStringParameters.token || false;
+    // if (token !== process.env.REBUILD_TOKEN) {
+    //   return {
+    //     statusCode: 200,
+    //     body: 'Invalid or missing token',
+    //   }
+    // }
 
-    // Check a token or something to prevent calls from anyone.
-
+    const now = new Date();
     const posts = fs.readdirSync('./_posts/');
     // Go through and check if there are dates in the future.
     // If we find any call netlify url to rebuild.
 
     return {
       statusCode: 200,
-      body: JSON.stringify({})
+      body: JSON.stringify({
+        now: now,
+        posts: posts,
+      })
     }
   } catch (err) {
     // Log errors somewhere?
